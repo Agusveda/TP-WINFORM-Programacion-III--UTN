f3bb03b3e3c61f4b9afac7b00aa036600119d53e
Facundo <124022262+FacundoDorrego@users.noreply.github.com>
2024-04-18 19:35:09 -0300

    Clase Imagenes
    Se agrego la clase imagenes

---
+++ Codigo/Clases/Imagenes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Imagenes
+    {
+
+        public int id;
+        public Articulo idArticulo; //Llama al objeto Articulo
+        public string ImagenURL;
+
+    }
+}
